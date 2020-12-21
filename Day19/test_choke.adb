80707c2adfddbe1d56932011898eaa9e4fb0e9dc
John Perry <john.perry@usm.edu>
2020-12-21 06:49:28 -0500

    removed a non-working experiment and added a source that illustrates a potential bug in GNAT

---
+++ Day19/test_choke.adb
@@ -0,0 +1,18 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+
+procedure Test_Choke is
+
+   S: String
+      := "29: 116 82 | 119 24";
+      -- := "29:";  -- OK
+      -- "29: "; -- NOT OK
+   A: Natural;
+   Last: Positive;
+
+begin
+
+   Get(S, A, Last);
+   Put(A, 0); New_Line;
+
+end test_choke;
